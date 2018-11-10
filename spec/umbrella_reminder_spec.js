5ea7f88ca7dd346e6c9434425988b09c98078e5a
Anja Istenic <aistenic1@gmail.com>
2018-11-10 14:08:22 +0100

    test written, umbrella_reminder.spec

---
+++ spec/umbrella_reminder_spec.js
@@ -0,0 +1,33 @@
+const assert = require("assert");
+const { getCity, getCityId, getRain } = require("../lib/umbrella_reminder");
+
+describe("Umbrella Reminder", () => {
+  // getCity should get user input (string) from form
+  describe("getCity", () => {
+    const city = getCity();
+    it('should return a string', () => {
+      assert.equal(typeof city, 'string');
+    });
+    it('should return capitalized string', () => {
+      assert.equal(city[0], city[0].toUpperCase());
+    });
+  });
+
+  describe("getCityId", () => {
+    // getKey should get city id from AccuWeather API
+    it("should return city id for given city", () => {
+      assert.equal(getKey('Agadir'), '245771');
+    });
+  });
+
+  describe("getRain", () => {
+    // getWeather should return a json file from api
+    const rainProbability = getRain();
+    it("should return rain probability for given city as Number", () => {
+      assert.equal(typeof rainProbability, 'number');
+    });
+  });
+});
+
+// should display 'you (don't) need an umbrella'
+// (should send push notification if umbrella is needed?)
